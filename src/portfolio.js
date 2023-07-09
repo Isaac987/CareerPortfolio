dec921505536b26c6be28cac66a73183acb84f31
Isaac Perkins <iperkins@uoregon.edu>
2023-07-08 21:36:24 -0700

    Added skills section

--- src/portfolio.js
+++ src/portfolio.js
@@ -47,15 +47,12 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  subTitle: "DEVELOPING CUTTING-EDGE SOFTWARE SOLUTIONS AND LEVERAGING MACHINE LEARNING TECHNIQUES TO SOLVE COMPLEX PROBLEMS",
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
-    ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
-    emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
-    )
+      "⚡ Develop cutting-edge software solutions"),
+    emoji("⚡ Utilize machine learning techniques to solve complex problems"),
+    emoji("⚡ Apply programming skills in languages such as C, C++, Python, and Java")
   ],
 
   /* Make Sure to include correct Font Awesome Classname to view your icon
